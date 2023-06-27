a85cc6b52ea9c47c8be178ceb6955437ce59761a
Victor Getsov <viktorgetsov@gmail.com>
2023-06-27 18:59:30 +0300

    add materialUI, react-hook-form, eslint-plugin-import, react-helmet-async, react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <App />
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 )
